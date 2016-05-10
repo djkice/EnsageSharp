0e1e1c371d1b77d0e7a4843cbd3dd6af5c26bc70
djkice <ckovaceski@gmail.com>
2016-05-10 20:39:56 +0800

    Added auto use ult

--- AlchemistSharp/Program.cs
+++ AlchemistSharp/Program.cs
@@ -7,27 +7,31 @@ using Ensage;
 using Ensage.Common;
 using Ensage.Common.Extensions;
 using Ensage.Common.Menu;
-using System.Timers;
-using System.IO;
 
 namespace AlchemistSharp
 {
     internal class Program
     {
-        private static Ability Stun;
+       
+        private static Ability rage;
         private static Item manta;
         private static Hero me;
         private static readonly Menu Menu = new Menu("AlchemistSharp", "AlchemistSharp", true, "npc_dota_hero_Alchemist", true);
 
-
-
         static void Main(string[] args)
         {
             Game.OnUpdate += Game_OnUpdate;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("| AlchemistSharp 1.1.0.0 Loaded |");
+            Console.WriteLine("---------------------------------");
+            Console.ResetColor();
             Game.OnWndProc += Game_OnWndProc;
-            var menuAlchemistSharp = new Menu("Cancel Stun", "opsi");
-            Menu.AddSubMenu(menuAlchemistSharp);
-            menuAlchemistSharp.AddItem(new MenuItem("enable", "Enable").SetValue(true));
+            var menuRage = new Menu("Auto Chemical Range", "opsi");
+            Menu.AddItem(new MenuItem("dodgetog", "Use Manta - Dodge Self Stun").SetValue(true));
+            menuRage.AddItem(new MenuItem("ragetog", "Use Chemical Range").SetValue(true));
+            menuRage.AddItem(new MenuItem("ragehealth", "If Health % is:").SetValue(new Slider(15)));
+            Menu.AddSubMenu(menuRage);
             Menu.AddToMainMenu();
         }
 
@@ -44,20 +48,33 @@ namespace AlchemistSharp
             if (manta == null)
                 manta = me.FindItem("item_manta");
 
-            var modifiers = me.Modifiers.ToList();
+            if (rage == null)
+                rage = me.Spellbook.SpellR;
+
+            var modifier = me.FindModifier("modifier_alchemist_unstable_concoction");
             float stunDur = 5.4f - (Game.Ping / 1000);
 
-            foreach (var modif in modifiers)
+            
+            if (modifier != null && modifier.ElapsedTime >= stunDur && Menu.Item("dodgetog").GetValue<bool>())
             {
-                    if (modif.Name == "modifier_alchemist_unstable_concoction" && modif.ElapsedTime >= stunDur && Menu.Item("enable").GetValue<bool>())
-                    {
-                        if (manta != null && manta.CanBeCasted() && Utils.SleepCheck("manta"))
-                        {
-                            manta.UseAbility();
-                            Utils.Sleep(150 + Game.Ping, "manta");
-                        }
-                    }
+                if (manta != null && manta.CanBeCasted() && Utils.SleepCheck("manta"))
+                {
+                    manta.UseAbility();
+                    Utils.Sleep(150 + Game.Ping, "manta");
+                }
             }
+
+            if (rage != null && rage.IsValid && rage.CanBeCasted() &&  Menu.Item("ragetog").GetValue<bool>() && me.Health <= me.MaximumHealth / 100 * Menu.Item("ragehealth").GetValue<Slider>().Value && Utils.SleepCheck("rage"))
+            {
+                 if (me.CanAttack() && me.CanCast() && !me.IsChanneling())
+                {
+                    rage.UseAbility();
+                    Utils.Sleep(250 + Game.Ping, "rage");
+                }
+               
+            }
+
+
         }
 
         private static void Game_OnWndProc(WndEventArgs args)
